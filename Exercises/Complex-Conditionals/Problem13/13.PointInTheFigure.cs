2b2105a54852e4358225bf766304e6ac58c664e4
Mustafa Yakupov <mustafa.yakupov97@gmail.com>
2018-03-09 09:55:35 +0200

    .

---
+++ Exercises/Complex-Conditionals/Problem13/13.PointInTheFigure.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PointInTheFigure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int h = int.Parse(Console.ReadLine());
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+
+            var insideHorisontalRectangular = (x > 0 && x < 3 * h) && (y > 0 && y < h);
+            var insideVerticalRectangular = (x > h && x < 2 * h) && (y > h && y < 4 * h);
+            var onMiddleSide = (x > h && x < 2 * h) && y == h;
+            var onBorder1 = ((x == 0 || x == 3 * h) && (y >= 0 && y <= h)) ||
+                             ((y == 0 || y == h) && (x >= 0 && x <= 3 * h));
+            var onBorder2 = ((x == h || x == 2 * h) && (y >= h && y <= 4 * h)) ||
+                             ((y == 4 || y == 4 * h) && (x >= h && x <= 2 * h));
+
+
+
+            if (insideHorisontalRectangular || insideVerticalRectangular || onMiddleSide)
+            {
+                Console.WriteLine("inside");
+            }
+            else if (onBorder1 || onBorder2)
+            {
+                Console.WriteLine("border");
+            }
+            else 
+            {
+                Console.WriteLine("outside");
+            }
+        }
+    }
+}
---
+++ Exercises/Complex-Conditionals/Problem2/02.SmallShop.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmallShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string product = Console.ReadLine();
+            string town = Console.ReadLine();
+            double quantity = double.Parse(Console.ReadLine());
+            //var result = 
+            if (town == "Sofia" && product == "coffee")
+            {
+                Console.WriteLine(quantity * 0.50);
+            }
+            if (town == "Sofia" && product == "water")
+            {
+                Console.WriteLine(quantity * 0.80);
+            }
+            if (town == "Sofia" && product == "beer")
+            {
+                Console.WriteLine(quantity * 1.20);
+            }
+            if (town == "Sofia" && product == "sweets")
+            {
+                Console.WriteLine(quantity * 1.45);
+            }
+            if (town == "Sofia" && product == "peanuts")
+            {
+                Console.WriteLine(quantity * 1.60);
+            }
+
+
+            if (town == "Plovdiv" && product == "coffee")
+            {
+                Console.WriteLine(quantity * 0.40);
+            }
+            if (town == "Plovdiv" && product == "water")
+            {
+                Console.WriteLine(quantity * 0.70);
+            }
+            if (town == "Plovdiv" && product == "beer")
+            {
+                Console.WriteLine(quantity * 1.15);
+            }
+            if (town == "Plovdiv" && product == "sweets")
+            {
+                Console.WriteLine(quantity * 1.30);
+            }
+            if (town == "Plovdiv" && product == "peanuts")
+            {
+                Console.WriteLine(quantity * 1.50);
+            }
+
+
+            if (town == "Varna" && product == "coffee")
+            {
+                Console.WriteLine(quantity * 0.45);
+            }
+            if (town == "Varna" && product == "water")
+            {
+                Console.WriteLine(quantity * 0.70);
+            }
+            if (town == "Varna" && product == "beer")
+            {
+                Console.WriteLine(quantity * 1.10);
+            }
+            if (town == "Varna" && product == "sweets")
+            {
+                Console.WriteLine(quantity * 1.35);
+            }
+            if (town == "Varna" && product == "peanuts")
+            {
+                Console.WriteLine(quantity * 1.55);
+            }
+        }
+    }
+}
